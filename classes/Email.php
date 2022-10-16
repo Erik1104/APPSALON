ebfde069704fba797d039e44fe211722acec4948
Erik1104 <steve110453@gmail.com>
2022-10-16 05:39:46 -0500

    modificando api

--- classes/Email.php
+++ classes/Email.php
@@ -37,7 +37,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p><strong> Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salon, solo debes confirmala presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aqui: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
